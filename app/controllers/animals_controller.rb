efc2b337867ea998c63202c9d9bc32f6442ceec9
annabellachan <annabellac24@gmail.com>
2022-12-13 15:06:28 -0800

    completed story 2, fixed indentations

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,28 +9,28 @@ class AnimalsController < ApplicationController
     end
     def create
         animal = Animal.create(animal_params)
-            if animal.valid?
-                render json: animal
-            else 
-                render json: animal.errors
-            end
+        if animal.valid?
+            render json: animal
+        else 
+            render json: animal.errors
+        end
     end
     def update
         animal = Animal.find(params[:id])
         animal.update(animal_params)
-            if animal.valid?
-                render json: animal
-            else    
-                render json: animal.errors
-            end
+        if animal.valid?
+            render json: animal
+        else    
+            render json: animal.errors
+        end
     end
     def destroy
         animal = Animal.find(params[:id])
-            if animal.destroy   
-                render json:animal
-            else    
-                render json: animal.errors
-            end
+        if animal.destroy   
+            render json:animal
+        else    
+            render json: animal.errors
+        end
     end
     private
     def animal_params
--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -9,28 +9,28 @@ class SightingsController < ApplicationController
     end
     def create
         sighting = Sighting.create(sighting_params)
-            if sighting.valid?
-                render json: sighting
-            else 
-                render json: sighting.errors
-            end
+        if sighting.valid?
+            render json: sighting
+        else 
+            render json: sighting.errors
+        end
     end
     def update
         sighting = Sighting.find(params[:id])
         sighting.update(sighting)
-            if sighting.valid?
-                render json: sighting
-            else    
-                render json: sighting.errors
-            end
+        if sighting.valid?
+            render json: sighting
+        else    
+            render json: sighting.errors
+        end
     end
     def destroy
         sighting = Sighting.find(params[:id])
-            if sighting.destroy   
-                render json:sighting
-            else    
-                render json: sighting.errors
-            end
+        if sighting.destroy   
+            render json:sighting
+        else    
+            render json: sighting.errors
+        end
     end
     private
     def sighting_params
